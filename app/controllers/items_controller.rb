14032f1f5d987ea393751b41e107476b5c1c53c4
akiko777 <natsu.hi.boshi731rainbowrose@gmail.com>
2021-02-09 21:23:36 +0900

    商品編集画面への遷移とルーティングを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,28 +2,30 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
 
-  def index
-    @items = Item.includes(:user).order("created_at DESC")
-  end
-
-  def new
-    @item = Item.new
-  end
-
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-       redirect_to root_path(@item)
-    else
-      render :new
+    def index
+      @items = Item.includes(:user).order("created_at DESC")
     end
-  end
+
+    def new
+      @item = Item.new
+    end
+
+    def create
+      @item = Item.new(item_params)
+      if @item.save
+         redirect_to root_path(@item)
+      else
+        render :new
+      end
+    end
+
     def show
       @item = Item.find(params[:id])
     end
 
-    #def edit
-    #end
+    def edit
+      @item = Item.find(params[:id])
+    end
 
     #def update
     #end
